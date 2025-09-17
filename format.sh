d20b0e17787055fec96ea322e00ba4cacb949629
Anya Lin <hukk1996@gmail.com>
2025-09-17 11:22:55 +0800

    [2.5.1] update to 2.5.1

--- format.sh
+++ format.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-for f in assets/minecraft/lang/*.json; do
-    raw="$(sed -E 's,(\\u(e|f[78])),\\\1,g' "$f")"
-    echo "$raw" | jq --sort-keys | sed 's|\\\\|\\|g' > "$f"
+for f in assets/minecraft/{lang,lang_rpo}/*.{json,rpo}; do
+    if [ -f "$f" ]; then
+        raw="$(sed -E 's,(\\u(e|f[78])),\\\1,g' "$f")"
+        echo "$raw" | jq --sort-keys | sed 's|\\\\|\\|g' > "$f"
+    fi
 done
